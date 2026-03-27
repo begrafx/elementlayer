20f0aca27c5181588adc8e238064a4cab0bf4ef6
BEGRAFX <brian@pixelsnstuff.com>
2026-03-27 11:10:01 -0400

    Add GitHub actions build pipeline

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'begrafx/elementlayer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd82243d69436d729a89dcd154a20f7d9bac976d7',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'begrafx/elementlayer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd82243d69436d729a89dcd154a20f7d9bac976d7',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.6',
+            'version' => '5.6.0.0',
+            'reference' => 'a2db6871deec989a74e1f90fafc6d58ae526a879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
